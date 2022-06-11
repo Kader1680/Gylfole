0430f0de16db2d2e1e362fbaf909b06bb3c6ab47
Kader1680 <ouldhenniabaghdad@gmail.com>
2022-06-11 21:39:07 +0100

    update js

--- js/script.js
+++ js/script.js
@@ -14,13 +14,21 @@ menu.addEventListener('click', ()=>{
             // faq section
 
 
-let faq = document.querySelectorAll('.faq');
+let faqs = document.querySelectorAll('.faq');
 let question = document.querySelector('.question');
 let answer = document.querySelector('.answer');
 let ans_block = document.querySelector('.ans_block');
 
-question.addEventListener('click', ()=>{
-    answer.classList.toggle('ans_block')
+// question.addEventListener('click', ()=>{
+//     answer.classList.toggle('ans_block')
 
     
+// })
+faqs.forEach(faq =>{
+    faq.addEventListener('click', ()=>{
+        faq.classList.toggle('ans_block')
+
+    })
 })
+
+
